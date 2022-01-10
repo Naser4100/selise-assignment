7992cd4beb77c4c06c6e7cd0602fff5faa61ae1a
Abdullah Naser <abdnaser.exe@gmail.com>
2022-01-10 20:25:05 +0600

    Change compile target to es5

--- build/config/example.default.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: 5000,
-    dbUri: '<YOUR_DB_URI>',
-};
--- build/src/middleware/asyncHandler.middleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const asyncHandler = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch((error) => {
-    next(error);
-    console.log(error);
-});
-exports.default = asyncHandler;
